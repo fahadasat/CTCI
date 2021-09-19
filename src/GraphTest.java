864a37a604164b6f45f2fe20d4ec0676c11a17a4
fahadasat <fahad.asat.242@my.csun.edu>
2021-09-19 11:13:10 -0700

    DFS w/ path

--- src/GraphTest.java
+++ src/GraphTest.java
@@ -1,7 +1,8 @@
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
+import java.util.*;
 
 public class GraphTest {
     public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class GraphTest {
         graph.addEdge(2, 1);
         graph.addEdge(3, 2);
         graph.addEdge(3, 4);
+//        graph.addEdge(5, 4);
 
         System.out.println(graph.printGraph());
 
@@ -22,48 +24,51 @@ public class GraphTest {
         System.out.println("Edges: " + graph.getEdgeCount());
         System.out.println();
 
-        System.out.println(graph.BFS(2, 5));
+        System.out.println("BFS " + graph.BFS(0, 2));
+
+        System.out.println("DFS " + graph.DFS(0, 2));
     }
 }
 
 
 class Graph {
-    private HashMap<Integer, LinkedList<Integer>> map = new HashMap<>();
+    private HashMap<Integer, LinkedList<Integer>> Graph = new HashMap<>();
 
-    //assuming undirected graph
+    //assuming directed graph
     public void addEdge(int source, int destination) {
-        if (!this.map.containsKey(source)) addVertex(source);
-        if (!this.map.containsKey(destination)) addVertex(destination);
+        if (!this.Graph.containsKey(source)) addVertex(source);
+        if (!this.Graph.containsKey(destination)) addVertex(destination);
 
-        if (!this.map.get(source).contains(destination)) this.map.get(source).add(destination);
-        if (!this.map.get(destination).contains(source)) this.map.get(destination).add(source);
+        if (!this.Graph.get(source).contains(destination)) this.Graph.get(source).add(destination);
+        //makes undirected graph
+//        if (!this.map.get(destination).contains(source)) this.map.get(destination).add(source);
     }
 
     public void addVertex(int vertex) {
         //check if vertex in graph
-        if (!this.map.containsKey(vertex)) {
-            this.map.put(vertex, new LinkedList<>());
+        if (!this.Graph.containsKey(vertex)) {
+            this.Graph.put(vertex, new LinkedList<>());
         }
     }
 
     public int getEdgeCount() {
         int edges = 0;
-        for (int key : this.map.keySet()) {
-            edges += this.map.get(key).size();
+        for (int key : this.Graph.keySet()) {
+            edges += this.Graph.get(key).size();
         }
         return edges / 2;
     }
 
     public int getVertexCount() {
-        return this.map.size();
+        return this.Graph.size();
     }
 
     public String printGraph() {
         StringBuilder string = new StringBuilder();
 
-        for (int key : this.map.keySet()) {
+        for (int key : this.Graph.keySet()) {
             string.append(key).append(": ");
-            for (int edge : this.map.get(key)) {
+            for (int edge : this.Graph.get(key)) {
                 string.append(edge).append(" ");
             }
             string.append("\n");
@@ -72,11 +77,10 @@ class Graph {
     }
 
     public String BFS(int vertexOne, int vertexTwo) {
-        if (!this.map.containsKey(vertexOne) || !this.map.containsKey(vertexTwo)) return "No Path";
+        if (!this.Graph.containsKey(vertexOne) || !this.Graph.containsKey(vertexTwo)) return "No Path";
         HashMap<Integer, Integer> parents = new HashMap<>();
-        HashSet<Integer> visited = new HashSet();
+        HashSet<Integer> visited = new HashSet<>();
         Queue<Integer> toVisit = new LinkedList<>();
-
 
         toVisit.add(vertexOne);
 
@@ -94,10 +98,10 @@ class Graph {
                 return "Shortest Path: " + string.reverse().toString().trim();
             }
 
-            for (int i = 0; i < this.map.get(current).size(); i++) {
-                if (!toVisit.contains(this.map.get(current).get(i)) && !visited.contains(this.map.get(current).get(i))) {
-                    toVisit.add(this.map.get(current).get(i));
-                    parents.put(this.map.get(current).get(i), current);
+            for (int i = 0; i < this.Graph.get(current).size(); i++) {
+                if (!toVisit.contains(this.Graph.get(current).get(i)) && !visited.contains(this.Graph.get(current).get(i))) {
+                    toVisit.add(this.Graph.get(current).get(i));
+                    parents.put(this.Graph.get(current).get(i), current);
                 }
             }
             visited.add(current);
@@ -105,7 +109,36 @@ class Graph {
         return "No Path";
     }
 
-    public boolean DFS() {
-        return false;
+    public String DFS(int vertexOne, int vertexTwo) {
+        if (!this.Graph.containsKey(vertexOne) || !this.Graph.containsKey(vertexTwo)) return "No Path";
+        Stack<Integer> toVisit = new Stack<>();
+        HashMap<Integer, Integer> parents = new HashMap<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        toVisit.push(vertexOne);
+
+        while (!toVisit.isEmpty()) {
+            int current = toVisit.pop();
+
+            if (current == vertexTwo) {
+                StringBuilder string = new StringBuilder();
+                string.append(vertexTwo).append(" ");
+                Integer value = vertexTwo;
+                while (value != null && parents.get(value) != null) {
+                    string.append(parents.get(value)).append(" ");
+                    value = parents.get(value);
+                }
+                return "Path: " + string.reverse().toString().trim();
+            }
+            for (int i = 0; i < this.Graph.get(current).size(); i++) {
+                if (!toVisit.contains(this.Graph.get(current).get(i)) && !visited.contains(this.Graph.get(current).get(i))) {
+                    toVisit.push(this.Graph.get(current).get(i));
+                    parents.put(this.Graph.get(current).get(i), current);
+                }
+            }
+            visited.add(current);
+        }
+
+        return "No Path";
     }
 }
