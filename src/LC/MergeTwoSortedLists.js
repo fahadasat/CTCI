3d5dbcb01a594c5cdc96a08a13bb46d36674c37f
Fahad Asat <fahad.asat.242@my.csun.edu>
2022-04-02 23:10:51 -0700

    lc: 21

---
+++ src/LC/MergeTwoSortedLists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * lc 21
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let combinedList = new ListNode();
+    let tail = combinedList;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            tail.next = list1;
+            list1 = list1.next;
+        } else {
+            tail.next = list2;
+            list2 = list2.next;
+        }
+        tail = tail.next;
+    }
+
+    if (list1) tail.next = list1;
+    else if (list2) tail.next = list2;
+
+    return combinedList.next;
+};
