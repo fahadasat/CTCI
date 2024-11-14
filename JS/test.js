c705803290bedc18ade43df55e98c49a0018cd08
fahadasat <asatfahad@gmail.com>
2024-11-13 23:19:08 -0700

    test

--- JS/test.js
+++ JS/test.js
@@ -1,62 +1,121 @@
-console.log("quick sort", quickSort([20, 4, 11, 14, 10, 8, 1, 5, 9, 6]))
-
-function quickSort(unsortedArray) {
-    quickSortRec(unsortedArray, 0, unsortedArray.length - 1);
-    return unsortedArray;
+class Node {
+    constructor(character) {
+        this.children = [];
+        this.letter = character;
+        this.count = 1;
+    }
 }
 
-function quickSortRec(unsortedArray, start, end) {
-    if (start >= end) return;
-    let pivot = partition(unsortedArray, start, end);
-    quickSortRec(unsortedArray, start, pivot - 1)
-    quickSortRec(unsortedArray, pivot + 1, end)
-
-}
-
-function partition(unsortedArray, start, end) {
-    let index = start;
-
-    for (let i = start; i < end; i++) {
-        if (unsortedArray[i] <= unsortedArray[end]) {
-            [unsortedArray[i], unsortedArray[index]] = [unsortedArray[index], unsortedArray[i]]
-            index++;
+class Trie {
+    constructor() {
+        this.letters = []
+    }
+    add(word) {
+        this.addRec(word, 0, this.letters);
+    }
+    addRec(word, index, trie) {
+        if(index >= word.length) {
+            return;
         }
+        if(trie[word.charCodeAt(index)]) {
+            trie[word.charCodeAt(index)].count += 1;
+        } else {
+            trie[word.charCodeAt(index)] = new Node(word[index]);
+        }
+        this.addRec(word, index + 1, trie[word.charCodeAt(index)].children);
     }
-    [unsortedArray[end], unsortedArray[index]] = [unsortedArray[index], unsortedArray[end]]
 
-    return index;
-}
-
-
-console.log("merge sort", mergeSort([4, 1, 5, 6, 10, 8, 20, 11, 9, 14]))
-
-function mergeSort(unsortedArray) {
-    if (unsortedArray.length < 2) return unsortedArray;
-
-    let pivot = Math.floor((unsortedArray.length - 1) / 2)
-
-    let left = mergeSort(unsortedArray.slice(0, pivot + 1))
-    let right = mergeSort(unsortedArray.slice(pivot + 1, unsortedArray.length))
-
-    return merge(left, right);
 
 }
 
-function merge(left, right) {
-    let output = [];
-
-    while (left.length > 0 && right.length > 0) {
-        if (left[0] <= right[0])
-            output.push(left.shift());
-        else
-            output.push(right.shift());
+longestCommonPrefix(["flower","flow","flight"]);
+function longestCommonPrefix  (strs) {
+    let trie = new Trie();
+    for(let i = 0; i < strs.length; i++) {
+        trie.add(strs[i]);
     }
+    let output = "";
 
-    output.push(...left);
-    output.push(...right);
+    let current = trie.letters;
+
+    do {
+        current.sort((a, b) => a.count - b.count);
+        console.log(current[0].letter, current[0].count)
+        if(current[0].count > 1) {
+            output += current[0].letter;
+        }
+        current = current[0].children;
+        current.sort((a, b) => b.count - a.count);
+    } while(current[0] != null && current[0].count > 1)
+    // console.log(trie.trie)
 
     return output;
 }
+
+
+
+
+
+
+// console.log("quick sort", quickSort([20, 4, 11, 14, 10, 8, 1, 5, 9, 6]))
+//
+// function quickSort(unsortedArray) {
+//     quickSortRec(unsortedArray, 0, unsortedArray.length - 1);
+//     return unsortedArray;
+// }
+//
+// function quickSortRec(unsortedArray, start, end) {
+//     if (start >= end) return;
+//     let pivot = partition(unsortedArray, start, end);
+//     quickSortRec(unsortedArray, start, pivot - 1)
+//     quickSortRec(unsortedArray, pivot + 1, end)
+//
+// }
+//
+// function partition(unsortedArray, start, end) {
+//     let index = start;
+//
+//     for (let i = start; i < end; i++) {
+//         if (unsortedArray[i] <= unsortedArray[end]) {
+//             [unsortedArray[i], unsortedArray[index]] = [unsortedArray[index], unsortedArray[i]]
+//             index++;
+//         }
+//     }
+//     [unsortedArray[end], unsortedArray[index]] = [unsortedArray[index], unsortedArray[end]]
+//
+//     return index;
+// }
+//
+//
+// console.log("merge sort", mergeSort([4, 1, 5, 6, 10, 8, 20, 11, 9, 14]))
+//
+// function mergeSort(unsortedArray) {
+//     if (unsortedArray.length < 2) return unsortedArray;
+//
+//     let pivot = Math.floor((unsortedArray.length - 1) / 2)
+//
+//     let left = mergeSort(unsortedArray.slice(0, pivot + 1))
+//     let right = mergeSort(unsortedArray.slice(pivot + 1, unsortedArray.length))
+//
+//     return merge(left, right);
+//
+// }
+//
+// function merge(left, right) {
+//     let output = [];
+//
+//     while (left.length > 0 && right.length > 0) {
+//         if (left[0] <= right[0])
+//             output.push(left.shift());
+//         else
+//             output.push(right.shift());
+//     }
+//
+//     output.push(...left);
+//     output.push(...right);
+//
+//     return output;
+// }
 
 //ben awad and clement medium google interview video question
 // function removeIslands(matrix) {
