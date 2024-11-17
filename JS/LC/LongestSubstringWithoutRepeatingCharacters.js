22c20b40e9a96dc7872e6abcf7171ae58caa2f74
fahadasat <asatfahad@gmail.com>
2024-11-16 22:20:39 -0700

    lc: 3 cleaner code

--- JS/LC/LongestSubstringWithoutRepeatingCharacters.js
+++ JS/LC/LongestSubstringWithoutRepeatingCharacters.js
@@ -2,32 +2,21 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
+// lc: 3
+var lengthOfLongestSubstring = function(s) {
+    let currentChars = new Set();
 
-    if (s.length === 1) return 1;
-
-    let letters = new Set();
-    let maxLength = 0;
-    let currentLength = 0;
-
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i; j < s.length; j++) {
-
-            if (!letters.has(s.charAt(j))) {
-                currentLength++;
-                letters.add(s.charAt(j));
-            } else {
-                letters = new Set();
-                if (currentLength > maxLength) {
-                    maxLength = currentLength;
-                }
-                currentLength = 0;
-                break;
-            }
+    let maxSize = 0;
+    let l = 0;
+    for(let i = 0; i < s.length; i++) {
+        while(currentChars.has(s[i]) && l <= i) {
+            currentChars.delete(s[l]);
+            l++;
         }
+        currentChars.add(s[i]);
+
+        maxSize = Math.max(maxSize, currentChars.size);
     }
-    if (currentLength > maxLength) {
-        maxLength = currentLength;
-    }
-    return maxLength;
+
+    return maxSize;
 };
